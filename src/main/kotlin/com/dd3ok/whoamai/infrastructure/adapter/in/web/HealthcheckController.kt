8163b8040b32371b304e263c02b6324a1ea01a8f
dd3ok <hwick@nate.com>
2025-06-25 23:48:03 +0900

    feat: alive를 위해 헬스체크 추가

---
+++ src/main/kotlin/com/dd3ok/whoamai/infrastructure/adapter/in/web/HealthcheckController.kt
@@ -0,0 +1,16 @@
+package com.dd3ok.whoamai.infrastructure.adapter.`in`.web
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/healthcheck")
+class HealthcheckController {
+
+    @GetMapping
+    fun healthcheck(): ResponseEntity<Void> {
+        return ResponseEntity.ok().build()
+    }
+}
